2459904337be251d6fa7404cc5751ef231e4817c
Codegn <guillermo.nicolas@gmail.com>
2019-10-01 12:01:32 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # torneo_nacional_calculator
+
+## Steps:
+
+1. install python (i recommend miniconda)
+2. pip install -r requirements.txt
+3. python torneo_nacional.py
--- torneo_nacional.py
+++ torneo_nacional.py
@@ -1,36 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
-import matplotlib
-
-
-# In[2]:
-
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('probs.csv', sep = ';', encoding = 'latin-1')
 
-
-# In[3]:
-
-
-df.tail(3)
-
-
-# In[4]:
-
-
 df_equipos = pd.read_csv('tabla_equipos_actual.csv', sep = ';', encoding = 'latin-1')
 equipos = list(df_equipos['EQUIPO'])
-equipos
-
-
-# In[5]:
-
 
 def suma_puntos(row):
     indice_local = equipos.index(row['LOCAL'])
@@ -43,11 +21,7 @@ def suma_puntos(row):
         puntajes[indice_local] += 1
         puntajes[indice_visita] += 1
 
-
-# In[6]:
-
-
-n_iter = 10000
+n_iter = 1000
 puntajes_guardados = []
 posiciones_guardadas = []
 for i in range(n_iter):
@@ -60,85 +34,24 @@ for i in range(n_iter):
     posiciones = list(s.rank(method='first', ascending = False))
     posiciones_guardadas.append(posiciones)
 
-
-# In[7]:
-
-
 puntajes_guardados_df = pd.DataFrame(puntajes_guardados, columns = equipos)
-
-
-# In[8]:
-
-
-puntajes_guardados_df.tail(3)
-
-
-# In[18]:
-
-
-boxplot = puntajes_guardados_df.boxplot(rot=90)
-
-
-# In[10]:
-
+ 
+puntajes_guardados_df[puntajes_guardados_df.columns[::-1]].boxplot(vert=False).get_figure().savefig('boxplot_puntajes.png', bbox_inches = "tight")
+plt.clf()
 
 posiciones_guardadas_df = pd.DataFrame(posiciones_guardadas, columns = equipos)
 
-
-# In[11]:
-
-
-posiciones_guardadas_df.tail(3)
-
-
-# In[12]:
-
-
-boxplot2 = posiciones_guardadas_df.boxplot(rot=90)
-
-
-# In[13]:
-
+posiciones_guardadas_df[posiciones_guardadas_df.columns[::-1]].boxplot(vert=False).get_figure().savefig('boxplot_posiciones.png', bbox_inches = "tight")
+plt.clf()
 
 df2 = pd.DataFrame(np.zeros((len(equipos), len(equipos))),columns=equipos)
 df2.index = range(1,1+len(equipos))
 
-
-# In[14]:
-
-
-df2.head()
-
-
-# In[15]:
-
-
 for index, row in posiciones_guardadas_df.iterrows():
     for equipo in list(df2.columns):
-        print(equipo)
         df2.at[int(row[equipo]), equipo] += 1
 
-
-# In[16]:
-
-
-df2
-
-
-# In[17]:
-
-
 df2.to_csv('matriz_resultados.csv', sep = ';', encoding = 'latin-1')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
