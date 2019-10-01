600f5a61a5aa38a3137686e7a6fed2375cbbb93d
Codegn <guillermo.nicolas@gmail.com>
2019-10-01 12:36:59 -0300

    update better plots and 10000 iter

--- torneo_nacional.py
+++ torneo_nacional.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 df = pd.read_csv('probs.csv', sep = ';', encoding = 'latin-1')
 
@@ -36,12 +37,18 @@ for i in range(n_iter):
 
 puntajes_guardados_df = pd.DataFrame(puntajes_guardados, columns = equipos)
 
-puntajes_guardados_df[puntajes_guardados_df.columns[::-1]].boxplot(vert=False).get_figure().savefig('boxplot_puntajes.png', bbox_inches = "tight")
+ax1 = puntajes_guardados_df[puntajes_guardados_df.columns[::-1]].boxplot(vert=False)
+ax1.xaxis.set_major_locator(ticker.MultipleLocator(2))
+plt.xticks(rotation=90)
+ax1.get_figure().savefig('boxplot_puntajes.png', bbox_inches = "tight")
 plt.clf()
 
 posiciones_guardadas_df = pd.DataFrame(posiciones_guardadas, columns = equipos)
 
-posiciones_guardadas_df[posiciones_guardadas_df.columns[::-1]].boxplot(vert=False).get_figure().savefig('boxplot_posiciones.png', bbox_inches = "tight")
+ax2 = posiciones_guardadas_df[posiciones_guardadas_df.columns[::-1]].boxplot(vert=False)
+ax2.xaxis.set_major_locator(ticker.MultipleLocator(1))
+plt.xticks(rotation=90)
+ax2.get_figure().savefig('boxplot_posiciones.png', bbox_inches = "tight")
 plt.clf()
 
 df2 = pd.DataFrame(np.zeros((len(equipos), len(equipos))),columns=equipos)
